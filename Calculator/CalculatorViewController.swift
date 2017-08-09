ae931ea2ddad7b9e80fc74ce19341160b073d3f8
Nahom Hailu <aymerenahom@gmail.com>
2017-08-09 15:25:10 +0300

    calculator modified and code cleaned up

---
+++ Calculator/CalculatorViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nahom Hailu on 26/09/16.
+//  Copyright (c) 2016 Nahom. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    
+    
+    @IBOutlet private var display: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    private var userIntheMiddleOfTyping = false
+    private var numberHasDot = false
+    private var operandIsVariable = false
+    private var brain = CalculatorBrain()
+    var savedProgram : CalculatorBrain.PropertyList?
+    
+    private var displayValue: Double? {
+        get {
+            if let value = Double(display.text!){
+                return value
+            } else {
+                return nil
+            }
+            /*
+             return display.text != nil ?
+             Double(display.text!): nil*/
+        }
+        set {
+            let formatter = NumberFormatter()
+            formatter.minimumSignificantDigits = 0
+            formatter.maximumFractionDigits = 6
+            display.text = newValue !=  nil ? formatter.string(from:NSNumber(value:newValue!)):nil
+        }
+    }
+
+    @IBAction func backspace(_ sender: UIButton) {
+        //deletes the last number from the display
+        if display.text != "" {
+            display.text?.remove(at: (display.text?.index(before: (display.text?.endIndex)!))!)
+        }
+    }
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
+    @IBAction private func touchOperand(_ sender: UIButton) {
+        let operand = sender.currentTitle!
+        if userIntheMiddleOfTyping {
+            let textCurrentlydisplayed = display.text!
+            display.text = textCurrentlydisplayed + operand
+
+            if operand == "." && !numberHasDot {
+                display.text = textCurrentlydisplayed + operand
+                numberHasDot = true
+            } else {
+                display.text = textCurrentlydisplayed + operand
+            }
+            
+        } else {
+            display.text = operand
+        }
+        userIntheMiddleOfTyping = true
+    }
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        
+        if userIntheMiddleOfTyping {
+            if displayValue != nil {
+                brain.setOperand(operand: displayValue!)
+                userIntheMiddleOfTyping = false
+                numberHasDot = false
+            }
+            
+        }
+        if let mathSymbol = sender.currentTitle {
+            brain.performOperation(symbol: mathSymbol)
+        }
+        if !brain.isResultPartial() {
+            descriptionLabel.text = brain.getDescription()
+        } else {
+            descriptionLabel.text = brain.getDescription() + "..."
+        }
+        
+        displayValue = brain.result
+    }
+}
+
--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -11,30 +11,17 @@ import Foundation
 
 class   CalculatorBrain
 {
-    private var isPartialResult = false;
+    private var isPartialResult = false
     private var description = " "
     private var accumulator = 0.0
     private var internalProgram = [AnyObject]()
-
-    func setOperand(operand:Double) {
-        accumulator = operand;
-        internalProgram.append(operand as AnyObject)
-    }
-    func getDescription()->String  {
-        return description
-    }
-
-  
-    func isResultPartial()->Bool {
-        return isPartialResult
-    }
-  
     private var operations:Dictionary<String,Operations> = [
         "π" : Operations.Constant(.pi), //M_PI,
         "e" : Operations.Constant(M_E), //M_E,
         "C" : Operations.Cancel, //cancel
         "√" : Operations.UnaryOperations(sqrt), //sqrt
         "cos" : Operations.UnaryOperations(cos), // cose
+        "sin" : Operations.UnaryOperations(sin), // sin
         "±" : Operations.UnaryOperations({-$0}), // change sign
         "%" : Operations.UnaryOperations({($0 * 0.01)}),
         "×" : Operations.BinaryOperations( * ),
@@ -43,7 +30,6 @@ class   CalculatorBrain
         "÷" : Operations.BinaryOperations( / ),
         "=" : Operations.Equals,
         "rand":Operations.RandomNumberGenerator
-        
     ]
     private enum Operations {
         case Constant(Double)
@@ -53,30 +39,73 @@ class   CalculatorBrain
         case Equals
         case Cancel
     }
-    func performOperation(symbol:String)  {
-        if let operation = operations[symbol]{
+    private var pending: PendingBinaryInfo?
+    private struct PendingBinaryInfo {
+        var binaryFunction: (Double,Double)->Double
+        var firstOperand: Double
+    }
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList {
+        get {
+            return internalProgram as CalculatorBrain.PropertyList
+        }
+        set {
+            clearMemory()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    }
+                    else if let operation = op as? String {
+                        performOperation(symbol: operation)
+                    }
+                }
+            }
+        }
+    }
+    var result:Double {
+        get {
+            return accumulator
+        }
+    }
+
+    func setOperand(operand:Double) {
+        accumulator = operand;
+        internalProgram.append(operand as AnyObject)
+    }
+    
+    func getDescription()->String {
+        return description
+    }
+    
+    func isResultPartial()->Bool {
+        return isPartialResult
+    }
+    
+    func performOperation(symbol:String) {
+        if let operation = operations[symbol] {
             internalProgram.append(symbol as AnyObject)
-            if(symbol == "rand"){
+            if symbol == "π" || symbol == "e" {
+                if description == "0" {
+                    description = String(format:"%g",accumulator)
+                }
+            } else  if symbol == "rand" {
                 description = "rand() = " + String(format:"%g",accumulator)
                 
-            }
-            else if(description != " ") {
+            } else  if description != " " {
                 let lastChar = description[description.index(before: description.endIndex)]
-                if((symbol == "×" ||  symbol == "÷") && (lastChar == "+" || lastChar == "-")||((symbol == "+" ||  symbol == "-")&&(lastChar == "×" || lastChar == "÷")) ){
+                if (symbol == "×" ||  symbol == "÷") && (lastChar == "+" || lastChar == "-") || ((symbol == "+" ||  symbol == "-") && (lastChar == "×" || lastChar == "÷")) {
                     description.insert("(", at: description.startIndex)
                     description.append(String(format:"%g",accumulator)+")"+symbol)
-                }
-                else if(lastChar == "="){
+                } else  if lastChar == "=" {
                     description = (String(format:"%g",accumulator)+symbol)
-                }
-                else{
+                } else {
                     description.append(String(format:"%g",accumulator)+symbol)
                 }
+            } else {
+                description.append(String(format:"%g",accumulator)+symbol)
             }
-  
-            else{
-                    description.append(String(format:"%g",accumulator)+symbol)
-                }
             
             switch operation {
             case .Constant(let associatedConstantValue):
@@ -95,22 +124,23 @@ class   CalculatorBrain
                 generateRandNum()
             }
         }
-        
     }
-    private func generateRandNum(){
+    
+    private func generateRandNum() {
         let rndNum = drand48();
         accumulator = (rndNum)
     }
-    private func clearMemory(){
+    
+    private func clearMemory() {
         accumulator = 0.0
         description = " "
         internalProgram.removeAll()
         pending = nil
         isPartialResult = false
-
     }
-    private func executePendingOperation(){
-        if(pending != nil){
+    
+    private func executePendingOperation() {
+        if pending != nil {
             accumulator = pending!.binaryFunction(pending!.firstOperand,accumulator)
             pending = nil
             isPartialResult = false
@@ -118,35 +148,4 @@ class   CalculatorBrain
         }
 
     }
-    private var pending: PendingBinaryInfo?
-    private struct PendingBinaryInfo {
-        var binaryFunction: (Double,Double)->Double
-        var firstOperand: Double
-    }
-    
-    typealias PropertyList = AnyObject
-    var program: PropertyList{
-        get{
-            return internalProgram as CalculatorBrain.PropertyList
-        }
-        set{
-            clearMemory()
-            if let arrayOfOps = newValue as? [AnyObject]{
-                for op in arrayOfOps{
-                    if let operand = op as? Double{
-                        setOperand(operand: operand)
-                    }
-                    else if let operation = op as? String{
-                        performOperation(symbol: operation)
-                    }
-                }
-            }
-        }
-    }
-    var result:Double{
-        get{
-            return accumulator
-        }
-    }
-    
 }
